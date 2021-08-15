30642ffc3c301ce694747bfa72b36305e8c65105
Rummer Stefan <qxz1i9m@europe.bmw.corp>
2021-08-15 20:47:13 +0200

    clean up code, install libraries

--- test_pipeline_kaggle.py
+++ test_pipeline_kaggle.py
@@ -1,75 +1,57 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing
-import os
-import tweepy as tw #for accessing Twitter API
 
+import pandas as pd  # data processing
 
-#For Preprocessing
-import re    # RegEx for removing non-letter characters
-import nltk  #natural language processing
-nltk.download("stopwords")
+import nltk  # natural language tool kit
+import pickle
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import *
 
-# For Building the model
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
-import seaborn as sns
 
-#For data visualization
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-%matplotlib inline
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+
+import tensorflow as tf
+import keras.backend as k
+
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, Conv1D, MaxPooling1D, Bidirectional, LSTM, Dense, Dropout
+from keras.metrics import Precision, Recall
+from keras.optimizers import SGD
+
+nltk.download("stopwords")
 pd.options.plotting.backend = "plotly"
 
 
 # Load Tweet dataset
-df1 = pd.read_csv('../input/twitter-and-reddit-sentimental-analysis-dataset/Twitter_Data.csv')
+df_tweets = pd.read_csv('/tweets_data/Tweets.csv')
+df_tweets = df_tweets.rename(columns={'text': 'clean_text', 'airline_sentiment': 'category'})
+df_tweets['category'] = df_tweets['category'].map({'negative': -1.0, 'neutral': 0.0, 'positive': 1.0})
+df_tweets = df_tweets[['category', 'clean_text']]
 # Output first five rows
-df1.head()
+df_tweets.head()
 
-# Load Tweet dataset
-df2 = pd.read_csv('../input/appletwittersentimenttexts/apple-twitter-sentiment-texts.csv')
-df2 = df2.rename(columns={'text': 'clean_text', 'sentiment':'category'})
-df2['category'] = df2['category'].map({-1: -1.0, 0: 0.0, 1:1.0})
-# Output first five rows
-
-df2.head()
-
-# Load Tweet dataset
-df3 = pd.read_csv('../input/twitterdata/finalSentimentdata2.csv')
-df3 = df3.rename(columns={'text': 'clean_text', 'sentiment':'category'})
-df3['category'] = df3['category'].map({'sad': -1.0, 'anger': -1.0, 'fear': -1.0, 'joy':1.0})
-df3 = df3.drop(['Unnamed: 0'], axis=1)
-# Output first five rows
-df3.head()
-
-# Load Tweet dataset
-df4 = pd.read_csv('../input/twitter-airline-sentiment/Tweets.csv')
-df4 = df4.rename(columns={'text': 'clean_text', 'airline_sentiment':'category'})
-df4['category'] = df4['category'].map({'negative': -1.0, 'neutral': 0.0, 'positive':1.0})
-df4 = df4[['category','clean_text']]
-# Output first five rows
-df4.head()
-
-df = pd.concat([df1, df2, df3, df4], ignore_index=True)
 
 # Check for missing data
-df.isnull().sum()
+df_tweets.isnull().sum()
 
 # drop missing rows
-df.dropna(axis=0, inplace=True)
+df_tweets.dropna(axis=0, inplace=True)
 
 # Map tweet categories
-df['category'] = df['category'].map({-1.0:'Negative', 0.0:'Neutral', 1.0:'Positive'})
+df_tweets['category'] = df_tweets['category'].map({-1.0: 'Negative', 0.0: 'Neutral', 1.0: 'Positive'})
 
 # Output first five rows
-df.head()
+df_tweets.head()
 
 
 def tweet_to_words(tweet):
-    ''' Convert tweet text into a sequence of words '''
 
     # convert to lowercase
     text = tweet.lower()
@@ -85,67 +67,60 @@ def tweet_to_words(tweet):
     return words
 
 
-print("\nOriginal tweet ->", df['clean_text'][0])
-print("\nProcessed tweet ->", tweet_to_words(df['clean_text'][0]))
+print("\nOriginal tweet ->", df_tweets['clean_text'][0])
+print("\nProcessed tweet ->", tweet_to_words(df_tweets['clean_text'][0]))
 
 # Apply data processing to each tweet
-X = list(map(tweet_to_words, df['clean_text']))
+X = list(map(tweet_to_words, df_tweets['clean_text']))
 
-from sklearn.preprocessing import LabelEncoder
 
 # Encode target labels
 le = LabelEncoder()
-Y = le.fit_transform(df['category'])
+Y = le.fit_transform(df_tweets['category'])
 
-y = pd.get_dummies(df['category'])
+y = pd.get_dummies(df_tweets['category'])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
-from sklearn.feature_extraction.text import CountVectorizer
-#from sklearn.feature_extraction.text import TfidfVectorizer
 
 vocabulary_size = 5000
 
 # Tweets have already been preprocessed hence dummy function will be passed in
 # to preprocessor & tokenizer step
 count_vector = CountVectorizer(max_features=vocabulary_size,
-#                               ngram_range=(1,2),    # unigram and bigram
-                                preprocessor=lambda x: x,
+                               preprocessor=lambda x: x,
                                tokenizer=lambda x: x)
-#tfidf_vector = TfidfVectorizer(lowercase=True, stop_words='english')
+# tfidf_vector = TfidfVectorizer(lowercase=True, stop_words='english')
 
 # Fit the training data
 X_train = count_vector.fit_transform(X_train).toarray()
-
 # Transform testing data
 X_test = count_vector.transform(X_test).toarray()
 
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
 
 max_words = 5000
-max_len=50
+max_len = 50
+
 
 def tokenize_pad_sequences(text):
-    '''
-    This function tokenize the input text into sequnences of intergers and then
-    pad each sequence to the same length
-    '''
+
+    # This function tokenize the input text into sequences of integers and then
+    # pad each sequence to the same length
+
     # Text tokenization
-    tokenizer = Tokenizer(num_words=max_words, lower=True, split=' ')
-    tokenizer.fit_on_texts(text)
+    tokenizer_padseq = Tokenizer(num_words=max_words, lower=True, split=' ')
+    tokenizer_padseq.fit_on_texts(text)
     # Transforms text to a sequence of integers
-    X = tokenizer.texts_to_sequences(text)
+    output = tokenizer_padseq.texts_to_sequences(text)
     # Pad sequences to the same length
-    X = pad_sequences(X, padding='post', maxlen=max_len)
+    output = pad_sequences(output, padding='post', maxlen=max_len)
     # return sequences
-    return X, tokenizer
+    return output, tokenizer_padseq
 
-print('Before Tokenization & Padding \n', df['clean_text'][0])
-X, tokenizer = tokenize_pad_sequences(df['clean_text'])
+
+print('Before Tokenization & Padding \n', df_tweets['clean_text'][0])
+X, tokenizer = tokenize_pad_sequences(df_tweets['clean_text'])
 print('After Tokenization & Padding \n', X[0])
-
-import pickle
 
 # saving
 with open('tokenizer.pickle', 'wb') as handle:
@@ -155,7 +130,7 @@ with open('tokenizer.pickle', 'wb') as handle:
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-y = pd.get_dummies(df['category'])
+y = pd.get_dummies(df_tweets['category'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
@@ -163,34 +138,23 @@ print('Train Set ->', X_train.shape, y_train.shape)
 print('Validation Set ->', X_val.shape, y_val.shape)
 print('Test Set ->', X_test.shape, y_test.shape)
 
-import keras.backend as K
 
-def f1_score(precision, recall):
-    f1_val = 2 * (precision * recall) / (precision + recall + K.epsilon())
+def f1_score(precision_val, recall_val):
+    f1_val = 2 * (precision_val * recall_val) / (precision_val + recall_val + k.epsilon())
     return f1_val
 
-from keras.models import Sequential
-from keras.layers import Embedding, Conv1D, MaxPooling1D, Bidirectional, LSTM, Dense, Dropout
-from keras.metrics import Precision, Recall
-from keras.optimizers import SGD
-from keras.optimizers import RMSprop
-from keras import datasets
 
-from keras.callbacks import LearningRateScheduler
-from keras.callbacks import History
-
-from keras import losses
-
-vocab_size = 5000
+# define essential variables
+vocab_size = 5000   # HYPERPARAMETER
 embedding_size = 32
-epochs=20
+epochs = 20
 learning_rate = 0.1
 decay_rate = learning_rate / epochs
 momentum = 0.8
 
+# BUILD the model
 sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
-# Build model
-model= Sequential()
+model = Sequential()
 model.add(Embedding(vocab_size, embedding_size, input_length=max_len))
 model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPooling1D(pool_size=2))
@@ -198,25 +162,21 @@ model.add(Bidirectional(LSTM(32)))
 model.add(Dropout(0.4))
 model.add(Dense(3, activation='softmax'))
 
-import tensorflow as tf
+# OUTPUT model information
 tf.keras.utils.plot_model(model, show_shapes=True)
-
 print(model.summary())
 
 # Compile model
 model.compile(loss='categorical_crossentropy', optimizer=sgd,
-               metrics=['accuracy', Precision(), Recall()])
+              metrics=['accuracy', Precision(), Recall()])
 
 # Train model
-
 batch_size = 64
-history = model.fit(X_train, y_train,
-                      validation_data=(X_val, y_val),
-                      batch_size=batch_size, epochs=epochs, verbose=1)
+history = model.fit(X_train, y_train, validation_data=(X_val, y_val),
+                    batch_size=batch_size, epochs=epochs, verbose=1)
 
-# Evaluate model on the test set
+# Evaluate model on the test set METRICS
 loss, accuracy, precision, recall = model.evaluate(X_test, y_test, verbose=0)
-# Print metrics
 print('')
 print('Accuracy  : {:.4f}'.format(accuracy))
 print('Precision : {:.4f}'.format(precision))
@@ -224,20 +184,20 @@ print('Recall    : {:.4f}'.format(recall))
 print('F1 Score  : {:.4f}'.format(f1_score(precision, recall)))
 
 
-def plot_training_hist(history):
-    '''Function to plot history for accuracy and loss'''
+def plot_training_hist(history_import):
+    # Function to plot history for accuracy and loss
 
     fig, ax = plt.subplots(1, 2, figsize=(10, 4))
     # first plot
-    ax[0].plot(history.history['accuracy'])
-    ax[0].plot(history.history['val_accuracy'])
+    ax[0].plot(history_import.history['accuracy'])
+    ax[0].plot(history_import.history['val_accuracy'])
     ax[0].set_title('Model Accuracy')
     ax[0].set_xlabel('epoch')
     ax[0].set_ylabel('accuracy')
     ax[0].legend(['train', 'validation'], loc='best')
     # second plot
-    ax[1].plot(history.history['loss'])
-    ax[1].plot(history.history['val_loss'])
+    ax[1].plot(history_import.history['loss'])
+    ax[1].plot(history_import.history['val_loss'])
     ax[1].set_title('Model Loss')
     ax[1].set_xlabel('epoch')
     ax[1].set_ylabel('loss')
@@ -247,20 +207,20 @@ def plot_training_hist(history):
 plot_training_hist(history)
 
 
-def plot_training_hist(history):
-    '''Function to plot history for accuracy and loss'''
+def plot_training_hist(history_import):
+    # Function to plot history for accuracy and loss
 
     fig, ax = plt.subplots(1, 2, figsize=(10, 4))
     # first plot
-    ax[0].plot(history.history['accuracy'])
-    ax[0].plot(history.history['val_accuracy'])
+    ax[0].plot(history_import.history['accuracy'])
+    ax[0].plot(history_import.history['val_accuracy'])
     ax[0].set_title('Model Accuracy')
     ax[0].set_xlabel('epoch')
     ax[0].set_ylabel('accuracy')
     ax[0].legend(['train', 'validation'], loc='best')
     # second plot
-    ax[1].plot(history.history['loss'])
-    ax[1].plot(history.history['val_loss'])
+    ax[1].plot(history_import.history['loss'])
+    ax[1].plot(history_import.history['val_loss'])
     ax[1].set_title('Model Loss')
     ax[1].set_xlabel('epoch')
     ax[1].set_ylabel('loss')
@@ -273,14 +233,12 @@ plot_training_hist(history)
 model.save('best_model.h5')
 print('Best model saved')
 
-from keras.models import load_model
-
 # Load model
 model = load_model('best_model.h5')
 
 
 def predict_class(text):
-    '''Function to predict sentiment class of the passed text'''
+    # Function to predict sentiment class of the passed text
 
     sentiment_classes = ['Negative', 'Neutral', 'Positive']
     max_len = 50
@@ -293,3 +251,6 @@ def predict_class(text):
     yt = model.predict(xt).argmax(axis=1)
     # Print the predicted sentiment
     print('The predicted sentiment is', sentiment_classes[yt[0]])
+
+
+predict_class("I've really enjoyed this flight to Cuba however the food was unsatisfying.")
